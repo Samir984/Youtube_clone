bdcb8eea923f3ed6d88053dabea94ab48ce837a0
Samir Nepane <sasuki984@gmail.com>
2023-11-30 11:24:08 +0545

    add node_modules to gitignore

--- src/pages/AppFeed.jsx
+++ src/pages/AppFeed.jsx
@@ -6,7 +6,7 @@ import Video, { videos } from "../component/Video";
 function AppFeed() {
   const { loading } = useVideo();
   return (
-    <div className="p-3 h-screen overflow-y-scroll">
+    <div className="p-3 h-[90vh] overflow-y-scroll">
       {loading && <Loader />}
       <div className="grid justify-items-center gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4">
         <Video video={videos.contents[0]} />
