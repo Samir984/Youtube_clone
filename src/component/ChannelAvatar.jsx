d8fe073609cdc44168ebff330f55223a7e472a15
Samir Nepane <sasuki984@gmail.com>
2023-11-30 11:59:47 +0545

    fix: padding

--- src/component/ChannelAvatar.jsx
+++ src/component/ChannelAvatar.jsx
@@ -1,7 +1,13 @@
 /* eslint-disable react/prop-types */
-function ChannelAvatar({ img }) {
+function ChannelAvatar({ img, type = "userAvatar" }) {
   return (
-    <img src={img} alt="user" className=" bg-black w-10 h-10 rounded-full max-[410px]:hidden sm:block " />
+    <img
+      src={img}
+      alt="user"
+      className={` bg-black w-10 h-10 rounded-full ${
+        type === "userAvatar" ? "max-[360px]:hidden" : ""
+      } sm:block `}
+    />
   );
 }
 
--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -16,7 +16,7 @@ function Video({ video }) {
 
         <div className="relative flex gap-4">
           <div className=" ">
-            <ChannelAvatar src={videoDetail.author.avatar[0].url} />
+            <ChannelAvatar src={videoDetail.author.avatar[0].url} type='channelAvatar'/>
           </div>
 
           <div className="absolute text-sm rounded-xl bg-slate-100 p-1 right-6 -top-12">
